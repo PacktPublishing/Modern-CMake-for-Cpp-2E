f8842e153ce1168a27ac2413aaf000077c7a1871
Drakemor <Drakemor@users.noreply.github.com>
2023-10-29 17:08:20 +0000

    Make chapter 12 example more messy for formatter

--- examples/ch12/01-formatting/src/main.cpp
+++ examples/ch12/01-formatting/src/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-using namespace std;
-int main() {
-  cout << "Hello, world!" << endl;
-}
+                               using namespace std;
+                       int main() {
+      cout << "Hello, world!" << endl;
+                                          }
