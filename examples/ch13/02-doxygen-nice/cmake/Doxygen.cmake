58b6c10a38e22d477541ff71a5d9b45d25879ff8
Drakemor <Drakemor@users.noreply.github.com>
2023-12-02 21:01:35 +0000

    Ch13: Add support for doxyygen-awesome extensions

--- examples/ch13/02-doxygen-nice/cmake/Doxygen.cmake
+++ examples/ch13/02-doxygen-nice/cmake/Doxygen.cmake
@@ -10,6 +10,7 @@ function(Doxygen input output)
   set(DOXYGEN_HTML_OUTPUT   ${PROJECT_BINARY_DIR}/${output})
 
   UseDoxygenAwesomeCss()
+  UseDoxygenAwesomeExtensions()
 
   doxygen_add_docs(doxygen
       ${PROJECT_SOURCE_DIR}/${input}
@@ -23,7 +24,7 @@ macro(UseDoxygenAwesomeCss)
     GIT_REPOSITORY
       https://github.com/jothepro/doxygen-awesome-css.git
     GIT_TAG
-      v1.6.0
+      v2.3.1
   )
   FetchContent_MakeAvailable(doxygen-awesome-css)
   set(DOXYGEN_GENERATE_TREEVIEW     YES)
@@ -32,4 +33,19 @@ macro(UseDoxygenAwesomeCss)
   set(DOXYGEN_DOT_TRANSPARENT       YES)
   set(DOXYGEN_HTML_EXTRA_STYLESHEET
       ${doxygen-awesome-css_SOURCE_DIR}/doxygen-awesome.css)
+endmacro()
+
+macro(UseDoxygenAwesomeExtensions)
+  set(DOXYGEN_HTML_EXTRA_FILES
+    ${doxygen-awesome-css_SOURCE_DIR}/doxygen-awesome-darkmode-toggle.js
+    ${doxygen-awesome-css_SOURCE_DIR}/doxygen-awesome-fragment-copy-button.js
+    ${doxygen-awesome-css_SOURCE_DIR}/doxygen-awesome-paragraph-link.js
+    ${doxygen-awesome-css_SOURCE_DIR}/doxygen-awesome-interactive-toc.js
+  )
+
+  execute_process(COMMAND doxygen -w html header.html footer.html style.css
+                  WORKING_DIRECTORY ${PROJECT_BINARY_DIR})
+  execute_process(COMMAND sed -i "/<\\/head>/r ${PROJECT_SOURCE_DIR}/cmake/extra_headers" header.html
+                  WORKING_DIRECTORY ${PROJECT_BINARY_DIR})
+  set(DOXYGEN_HTML_HEADER ${PROJECT_BINARY_DIR}/header.html)
 endmacro()
