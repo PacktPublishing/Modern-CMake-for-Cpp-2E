8d819ac1bd120538ed70cd99c65f4170fcd36714
Drakemor <Drakemor@users.noreply.github.com>
2024-03-22 14:14:13 +0000

    Ch14: Add module support to the export() example

---
+++ examples/ch14/00-test-export/main.cpp
@@ -0,0 +1,8 @@
+import extended;
+
+#include <iostream>
+
+int main() {
+  std::cout << "Multiplication 2 * 2 = " << Multiply(2, 2) << std::endl;
+  return 0;
+}
---
+++ examples/ch14/01-export/src/basic.cpp
@@ -0,0 +1,9 @@
+#include "calc/basic.h"
+
+int Add(int a, int b) {
+  return a + b;
+}
+
+int Subtract(int a, int b) {
+  return a - b;
+}
