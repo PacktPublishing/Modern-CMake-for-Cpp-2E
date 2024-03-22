9b0776d1416512f9ce17218a65e5bf035605dfd6
Drakemor <Drakemor@users.noreply.github.com>
2024-03-22 18:33:42 +0000

    Ch14: Fix the export() behavior

--- examples/ch14/00-test-export/main.cpp
+++ examples/ch14/00-test-export/main.cpp
@@ -1,8 +1,10 @@
 import extended;
 
 #include <iostream>
+#include "calc/basic.h"
 
 int main() {
+  std::cout << "Addition 2 + 2 = " << Add(2, 2) << std::endl;
   std::cout << "Multiplication 2 * 2 = " << Multiply(2, 2) << std::endl;
   return 0;
 }
